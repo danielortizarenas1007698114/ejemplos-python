6992656c58b68f289c60c64cb1adb46ec2198090
daniel <danielortizarenas@gmail.com>
2019-08-08 17:42:16 -0500

    punto 10 diccionario

--- ejercicio python/punto10diccionario.py
+++ ejercicio python/punto10diccionario.py
@@ -1,5 +1,10 @@
 #diccionario puede gusrdar mucho tipo de informacion
-persona = {'nombre' : 'Daniel', 'apellido' : 'Ortiz', 'Edad' : '19', 'cursos' : ['PSeint','Java','C#','html','JavaScript','Python']}
+persona = {
+    'nombre' : 'Daniel',
+    'apellido' : 'Ortiz',
+    'Edad' : '19',
+    'cursos' : ['PSeint','Java','C#','html','JavaScript','Python']
+    }
 
 for datos in persona:
     print(datos,": ",persona[datos])
